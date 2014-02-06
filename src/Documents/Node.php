b1559882b5a1922e79cf973e9264674a4b747da7
lorello <lorenzo.salvadorini@softecspa.it>
2014-02-06 16:55:44 +0100

    Nodes documents now ereditates from Base

--- src/Documents/Node.php
+++ src/Documents/Node.php
@@ -2,21 +2,32 @@
 
 namespace Documents;
 
-class Node extends \Purekid\Mongodm\Model
+class Node extends \Documents\Base
 {
     static $collection = "nodes";
 
     protected static $attrs = array(
-        'hostname'   => array('type' => 'string'),
+        'hostname'   => array('type' => 'string', 'null' => false),
         'fqdn'       => array('type' => 'string'),
-        'lastupdate' => array('type' => 'timestamp')
+        'lastupdate' => array('type' => 'timestamp', 'autoupdate' => true)
     );
+    /*
+        function __preSave()
+        {
+            // Implemented in Document\Base
+            // $date = new \DateTime();
+            // $this->__setter('lastupdate', $date->getTimestamp());
 
-    function __preSave()
-    {
-        $date = new \DateTime();
-        $this->__setter('lastupdate', $date->getTimestamp());
+            // Implemented in Document\Base
+    //        foreach(self::$attrs as $key=>$attr){
+    //            if (isset($attr['null']) and ($attr['null']===FALSE)) {
+    //                if (!isset($this->cleanData[$key]) or empty($this->cleanData[$key])) {
+    //                    throw new \Exception("The field '$key' cannot be empty");
+    //                }
+    //            }
+    //        }
 
-        return parent::__preSave();
-    }
+            return parent::__preSave();
+        }
+    */
 }
