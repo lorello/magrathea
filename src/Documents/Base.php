3e269250e21c636332b59650b0358ec5b5adc290
lorello <lorenzo.salvadorini@softecspa.it>
2014-02-06 19:34:19 +0100

    Extended Base Document features: can now manage timestamp autoupdated fields and regexp checks for strings

--- src/Documents/Base.php
+++ src/Documents/Base.php
@@ -7,19 +7,33 @@ class Base extends \Purekid\Mongodm\Model
 
     function __preSave()
     {
-        foreach (self::$attrs as $key => $attr) {
+        $attrs = self::getAttrs();
 
+        foreach ($attrs as $key => $attr) {
+
+            $value  = $this->__get($key);
             // Manage field attribute NOT NULL
             if (isset($attr['null']) and ($attr['null'] === false)) {
-                if (!isset($this->cleanData[$key]) or empty($this->cleanData[$key])) {
+                if (!$this->__isset($key) or empty($value)) {
                     throw new \Exception("The field '$key' cannot be empty");
                 }
             }
 
-            // Manage filed attribute 'autoupdate'
-            if ($attr['type'] == self::DATA_TYPE_TIMESTAMP and isset($attr['autoupdate']) and ($attr['autoupdate'] === true)) {
+            // Manage field attribute 'autoupdate'
+            if ((isset($attr['type']) and $attr['type'] == self::DATA_TYPE_TIMESTAMP) and
+                isset($attr['autoupdate']) and ($attr['autoupdate'] === true)
+            ) {
                 $date                  = new \DateTime();
-                $this->cleanData[$key] = $date->getTimestamp();
+                $ts = $date->getTimestamp();
+                $this->__set($key, $ts);
+            }
+
+            // Manage field attribute 'regexp'
+            if ((isset($attr['type']) and $attr['type'] == self::DATA_TYPE_STRING) and isset($attr['regexp'])) {
+                $filter_options = array('options' => array('regexp' => '/' . $attr['regexp'] . '/'));
+                if (!filter_var($value, FILTER_VALIDATE_REGEXP, $filter_options)) {
+                    throw new \Exception("The field '$key' doesn't validate against expression '$attr[regexp]'");
+                }
             }
         }
 
