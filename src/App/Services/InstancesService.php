1662a41b8b958458ff0627a162ef97bb29d7177d
lorello <lorenzo.salvadorini@softecspa.it>
2014-03-20 08:34:03 +0100

    Created service for Instances on Cluster

---
+++ src/App/Services/InstancesService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use Documents\Instance;
+
+class InstancesService extends BaseService
+{
+    public function get($id)
+    {
+        $item = Instance::id($id);
+        if (!($item instanceof Instance)) {
+            throw new \Exception("Cannnot find instance with id '$id''");
+        }
+
+        return $item;
+    }
+
+    public function getByName($value)
+    {
+        return Instance::one(array('name' => $value));
+    }
+
+    public function getAll()
+    {
+        $items  = Instance::all();
+        $result = array();
+        foreach ($items as $item) {
+            $result[] = array(
+                'id'      => (string)$item->getId(),
+                'name'    => $item->getName(),
+                'app'     => $item->getApp(),
+                'cluster' => $item->getCluster()
+            );
+        }
+
+        return $result;
+    }
+
+    public function save($data)
+    {
+        $item = new Instance();
+        $item->setName($data['name']);
+        $item->setApp($data['app']);
+        $item->setCluster($data['cluster']);
+        $item->save();
+
+        // return the MongoId object
+        return $item->getId();
+    }
+
+    public function update($id, $data)
+    {
+        $item = Instance::id($id);
+        if (!$item) {
+            throw new \Exception("Item with id '$id' not found");
+        }
+        $item->update($data);
+
+        return $item->save();
+    }
+
+    public function delete($id)
+    {
+        $item = Instance::id($id);
+        if (!$item) {
+            throw new \Exception("Instance with id '$id' not found");
+        }
+
+        return $item->delete();
+    }
+}
