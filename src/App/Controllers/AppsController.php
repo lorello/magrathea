f965b8bebd0714d8ec98c31b463ca8b9a230d4ec
lorello <lorenzo.salvadorini@softecspa.it>
2014-03-20 08:36:01 +0100

    Added connect and deploy methods

--- src/App/Controllers/AppsController.php
+++ src/App/Controllers/AppsController.php
@@ -12,9 +12,19 @@ class AppsController extends BaseController
     public function getDataFromRequest(Request $request)
     {
         return array(
-            'name'  => $request->request->get('name'),
-            'owner' => $request->request->get('owner'),
-            'conf'  => $request->request->get('conf'),
+            'name'     => $request->request->get('name'),
+            'conf'     => $request->request->get('conf'),
+            'username' => $this->getCurrentUser()
         );
+    }
+
+    public function connect($id, $instance_id)
+    {
+        $this->service->connect($id, $instance_id)
+    }
+
+    public function deploy($id, $instance_id)
+    {
+
     }
 }
