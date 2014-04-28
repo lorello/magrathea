0716dd596e57963759845d596672912eaf677c41
lorello <lorenzo.salvadorini@softecspa.it>
2014-04-28 18:03:08 +0200

    Minor changes to documents

--- src/Documents/Cluster.php
+++ src/Documents/Cluster.php
@@ -9,6 +9,7 @@ class Cluster extends \Documents\Base
     protected static $attrs = array(
         'name'   => array('type' => 'string', 'regexp' => '[a-z][a-z0-9]{2,}', 'null' => false),
         'layers' => array('type' => 'embeds', 'model' => 'Documents\Layer'),
+        'layer' => array('type' => 'embed', 'model' => 'Documents\Layer'),
         'owner'  => array('type' => 'reference', 'model' => 'Documents\User', 'null' => false),
         'lastupdate' => array('type' => 'timestamp', 'autoupdate' => true)
     );
--- src/Documents/Layer.php
+++ src/Documents/Layer.php
@@ -7,8 +7,6 @@ namespace Documents;
 class Layer extends \Documents\Base
 {
 
-    #static $collection = "layer";
-
     protected static $attrs = array(
         'name'       => array('type' => 'string', 'regexp' => '[a-z][a-z0-9]{4,}', 'null' => false),
         #'nodes'      => array( 'type' => 'references', 'model' => 'Documents\Node'),
